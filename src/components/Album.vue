4ff59deec100db4f948de006bffa3493cbfa68cb
Henry Quinn <henryquinniv@gmail.com>
2019-02-13 22:29:52 -0500

    Getting initial static version up on Heroku.

---
+++ src/components/Album.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="album-details">
+    <img :alt="title + ' by ' + band" :src="getImgUrl(cover)" />
+    <span class="caption">
+      <h1>{{ title }}</h1>
+      <h3>{{ band }}</h3>
+      <a class="listenLink" :href="spotifyLink"><i class="fab fa-spotify fa-2x"></i></a>
+      <a class="listenLink" :href="itunesLink"><i class="fab fa-itunes fa-2x"></i></a>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Album",
+  props: {
+    cover: String,
+    title: String,
+    band: String,
+    spotifyLink: String
+  },
+  methods: {
+    getImgUrl(pic) {
+      return require("../assets/album_covers/" + pic);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div.album-details {
+  vertical-align: top;
+  display: inline-block;
+  text-align: left;
+  width: 220px;
+  margin: 0 10 0 10;
+}
+img {
+  width: 200px;
+  height: 200px;
+  background-color: grey;
+}
+.caption {
+  display: block;
+}
+h1 {
+  margin: -5px 0px -5px 0px;
+}
+h3 {
+  margin: 5px 0px 5px 0px;
+}
+a {
+  color: #0099cb;
+}
+.listenLink {
+  margin: 0px 10px 0px 0px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="hello">
+      <h1>/Blo͞o ˈAlbəm/</h1>
+      <h2><i>noun</i></h2>
+      <!-- eslint-disable-next-line -->
+      <h3>1. An album that can be listened to in it's entirety, front to back, without skipping a single track.</h3>
+    </div>
+    <!-- eslint-disable-next-line -->
+    <Album cover="weezer-blue_album.jpeg" 
+    title="Blue Album" 
+    band="Weezer" 
+    spotifyLink="https://open.spotify.com/album/1xpGyKyV26uPstk1Elgp9Q?si=gR_B1thgTmuP0mVTodxfdA" 
+    itunesLink="https://geo.itunes.apple.com/us/album/weezer/1136784464?mt=1&app=music" />
+    <!-- eslint-disable-next-line -->
+    <Album cover="tmbg-flood.png" 
+    title="Flood" 
+    band="They Might Be Giants" 
+    spotifyLink="https://open.spotify.com/album/7FwAtuhhWivxvK4aPgyyUD?si=-2VKW6syTD-STaDETqcTVQ" 
+    itunesLink="https://geo.itunes.apple.com/us/album/flood/298111036?mt=1&app=music" />
+    <!-- eslint-disable-next-line -->
+    <Album cover="rhcp-blood_sugar_sex_magik.png" 
+    title="Blood Sugar Sex Magik" 
+    band="Red Hot Chili Peppers" 
+    spotifyLink="https://open.spotify.com/album/30Perjew8HyGkdSmqguYyg?si=n88VNfm0Skud7MG95OPyEQ" 
+    itunesLink="https://geo.itunes.apple.com/us/album/blood-sugar-sex-magik-deluxe-version/948460531?mt=1&app=music" />
   </div>
 </template>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #0099cb;
+}
+</style>
+
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Album from "@/components/Album.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Album
   }
 };
 </script>
